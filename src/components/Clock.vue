38a9ec1e92178f4c0d243c1db0daa929b556955d
allworldg <944937881@qq.com>
2024-06-09 10:59:43 +0800

    init tailwindcss , init clockView

---
+++ src/components/Clock.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex items-center">
+    <span class="text-5xl">{{ formatTime }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps<{ remainSeconds: number }>();
+const formatTime = computed(() => {
+  const minutes = Math.trunc(props.remainSeconds / 60);
+  const seconds = Math.trunc(props.remainSeconds % 60);
+  return `${minutes}:${seconds}`;
+});
+</script>
+
+<style></style>
